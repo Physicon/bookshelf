19235a0ce3845334eb34113d4bc1f5904e207904
Armen Avetisyan <armenavetisyan.sa@gmail.com>
2019-04-09 16:50:00 +0300

    style, pipe improvements

--- src/app/pipes/book-filter.pipe.ts
+++ src/app/pipes/book-filter.pipe.ts
@@ -14,7 +14,7 @@ export class BookFilterPipe implements PipeTransform {
       value = value.filter(b => b.genre === selectedGenre)
     }
     if (selectedClass !== 'none') {
-      value = value.filter(b => b.grade.includes(selectedClass))
+      value = value.filter(b => (b.grade === selectedClass) || (b.grade.includes(';'+selectedClass+';')) || (b.grade.includes(selectedClass+';') || (b.grade.includes(';'+selectedClass))))
     }
     if (query.trim()) {
       value = value.filter(b => b.title.toLowerCase().includes(query.trim().toLowerCase()))
